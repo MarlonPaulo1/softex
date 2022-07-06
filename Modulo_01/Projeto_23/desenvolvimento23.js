f08310d0f2774815d33addc00de531bdc80115ab
Marlon Paulo <marlonpaulo1@gmail.com>
2022-07-06 01:09:16 -0300

    vigésimo quarto projeto módulo 1

--- Modulo_01/Projeto_23/desenvolvimento23.js
+++ Modulo_01/Projeto_23/desenvolvimento23.js
@@ -1,3 +1,13 @@
+// Crie uma classe e insira nela, no mínimo, dois atributos, 
+//os quais devem ter um método acessor (get) e um método modificador (set) para cada.
+//Defina um objeto para cada atributo e elabore um construtor para criar alguma regra.
+
+// A atividade pode ser realizada em qualquer linguagem de programação ou apenas 
+// utilizando algoritmos.
+
+// Trabalhe esse código em seu IDE ou no Bloco de Notas, e compartilhe o link no
+// campo ao lado para que outros desenvolvedores possam analisá-lo. 
+
 class Clube {
     constructor(nome, cores , anoDeFundacao){
         this.nome = nome,
---
+++ Modulo_01/Projeto_24/readme.md
@@ -0,0 +1,7 @@
+Crie dois pacotes baseados nas atribuições de uma empresa e
+inclua classes neles. Lembre-se de trabalhar com os padrões
+Java.
+
+Realize essa atividade no WORD ou no Bloco de Notas, suba esse 
+arquivo para algum repositório e compartilhe o link no campo ao 
+lado para que outros desenvolvedores possam analisá-lo.
