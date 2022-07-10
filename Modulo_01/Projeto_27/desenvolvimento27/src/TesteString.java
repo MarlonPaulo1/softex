7748211048328b4b2f1dde448bb4cc4316f478af
Marlon Paulo <marlonpaulo1@gmail.com>
2022-07-10 10:54:51 -0300

    vigésimo oitavo projeto módulo 1

--- Modulo_01/Projeto_27/desenvolvimento27/src/TesteString.java
+++ Modulo_01/Projeto_27/desenvolvimento27/src/TesteString.java
@@ -1,3 +1,11 @@
+// Crie uma situação em que ocorra uma exceção dentro de um código. 
+//Utilize try/catch para realizar a captura e tratamento dessa exceção. 
+
+// Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e compartilhe o link 
+//desse projeto no campo ao lado para que outros desenvolvedores possam analisá-lo.
+
+
+
 public class TesteString {
     private static void aumentarLetras() throws NullPointerException {
       String frase = null;
