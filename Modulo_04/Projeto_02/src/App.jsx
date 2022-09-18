b4b52d20f423b294ea23d6569c8713767870a59c
Marlon Paulo <marlonpaulo1@gmail.com>
2022-09-18 19:49:10 -0300

    Segundo projeto módulo 4

---
+++ Modulo_04/Projeto_02/src/App.jsx
@@ -0,0 +1,12 @@
+import MyName from "./components/MyName";
+import './styles/global.css'
+
+function App() {
+  return (
+    <div className="App">
+      <MyName />
+    </div>
+  );
+}
+
+export default App;
---
+++ Modulo_04/Projeto_02/src/components/MyName/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+function MyName() {
+  return <h2 data-text="&nbsp;Marlon Paulo&nbsp;">&nbsp;Marlon Paulo&nbsp;</h2>
+}
+
+export default MyName
